35da3f4f4d45a705752f984bb6ed5328988c38aa
JohnPac <johnpac15@yahoo.com>
2022-01-14 15:49:36 -0600

    refactored the api routes to work with get and post calls

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const fs = require("fs");
 const path = require("path");
 
-const notes = require("./db/db");
 const express = require("express");
+const database = require("mime-db");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -12,12 +12,59 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
+// const notes = require("./db/db");
 
 app.get("/api/notes", (req, res) => {
-  let results = notes;
-  console.log(req.query,'sup');
-  res.json(results);
+  fs.readFile("./db/db.json", "utf-8", (err, data) => {
+    if (err) throw err;
+    let notes = JSON.parse(data);
+    // console.log(notes, "sup");
+    return res.json(notes);
+  });
+  //   let results = notes;
 });
+
+app.post("/api/notes", (req, res) => {
+  console.log(req.body)
+
+  const {title, text} = req.body
+
+  const newNote = {
+    title,
+    text
+  }
+    
+  fs.readFile('./db/db.json', 'utf-8', (err, data) => {
+    if(err) throw err
+    //covert data from string into JSON object
+    console.log('readfile 1', data)
+    const parseData = JSON.parse(data)
+    console.log('readfile 2',parseData)
+    //add the new note
+    // const noteArray = []
+    // noteArray.push(parseData)
+    parseData.push(newNote)
+    console.log('readfile 3',parseData)
+    //turn all the new data into a string for write file to work
+    const stringNote = JSON.stringify(parseData)
+    console.log('readfile 4'),stringNote
+
+
+    // write the updates notes back to the file
+    fs.writeFile(
+      './db/db.json',
+        stringNote, 
+        (writeErr) => writeErr
+          ? console.error(writeErr)
+          : console.log("updated note successfully"),
+    )
+
+  }) 
+  res.json()
+})
+
+
+// });
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "./public/index.html"));
