da6afe46e2bdb67c953d192889fcd7683e756ab6
JohnPac <johnpac15@yahoo.com>
2022-01-16 15:51:11 -0600

    changed the Og json file to a empty []

--- server.js
+++ server.js
@@ -15,13 +15,31 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
+const writeFile = function (stringData) {
+  // write the updates notes back to the file
+  fs.writeFile("./db/db.json", stringData, (writeErr) =>
+    writeErr
+      ? console.error(writeErr)
+      : console.log("---updated note successfully---")
+  );
+};
+
 app.get("/api/notes", (req, res) => {
   fs.readFile("./db/db.json", "utf-8", (err, data) => {
-    if (err) throw err;
-    let notes = JSON.parse(data);
-    return res.json(notes);
+    const noteParse = JSON.parse(data)
+    console.log('---GET 1---', noteParse)
+    if (noteParse == []) {
+      console.log('---GET 2---', noteParse)
+      return res.json(noteParse);
+    } else {
+      if (err) {
+        throw err;
+      } else {
+        console.log('---GET 3---', noteParse)
+        return res.json(noteParse);
+      }
+    }
   });
-  //   let results = notes;
 });
 
 app.post("/api/notes", (req, res) => {
@@ -37,37 +55,50 @@ app.post("/api/notes", (req, res) => {
   //reading what is currently on the db.json file
   fs.readFile("./db/db.json", "utf-8", (err, data) => {
     if (err) throw err;
-
+    
     //covert data from the db.json file from a string into JSON object
-    console.log("---readfile 1---", data);
     const parseData = JSON.parse(data);
-    console.log("---readfile 2---", parseData[0]);
 
-    //add the new noteNote we made above to the db.json array
-    parseData.push(newNote);
-    console.log("---readfile 3---", parseData);
+    console.log("---readfile 1---", data);
+    if (data == []) {
+      //add the new noteNote we made above to the db.json array
+      parseData.push(newNote);
+      console.log("---readfile 2 if data = []---", parseData);
 
-    // adds a id to each new note
-    parseData.forEach((item, i) => {
-      item.id = i + 0;
-    });
-    console.log("---readfile add id---", parseData);
+      // adds a id to each new note
+      parseData.forEach((item, i) => {
+        item.id = i;
+      });
+      console.log("---readfile 3 add id---", parseData);
 
-    //turn all the new data into a string for write file to work
-    const stringNote = JSON.stringify(parseData);
-    console.log("---readfile 4---", stringNote);
+      const stringNote = JSON.stringify(parseData);
+      console.log("---readfile 4---", stringNote);
 
-    // write the updates notes back to the file
-    fs.writeFile("./db/db.json", stringNote, (writeErr) =>
-      writeErr
-        ? console.error(writeErr)
-        : console.log("---updated note successfully---")
-    );
+      return writeFile(stringNote);
+
+    } else {
+
+      console.log("---readfile 5---", parseData);
+
+      //add the new noteNote we made above to the db.json array
+      parseData.push(newNote);
+      console.log("---readfile 6---", parseData);
+
+      // adds a id to each new note
+      parseData.forEach((item, i) => {
+        item.id = i;
+      });
+      console.log("---readfile 7 add id ---", parseData);
+
+      //turn all the new data into a string for write file to work
+      const stringNote = JSON.stringify(parseData);
+      console.log("---readfile 8 ---", stringNote);
+      return writeFile(stringNote);
+    }
   });
 });
 
 app.delete(`/api/notes/:number`, (req, res) => {
-
   fs.readFile("./db/db.json", "utf-8", (err, data) => {
     if (err) throw err;
     //the is the db.json file in a Object format
@@ -78,29 +109,29 @@ app.delete(`/api/notes/:number`, (req, res) => {
 
     //this is the parsed version of the id of the note i want to delete
     const x = JSON.parse(indexId);
-    console.log(x, '---delete 1---', parseData)
+    console.log(x, "---delete 1---", parseData);
 
     //this is going to delete the note from the db.json Object
     let spliceSelectedIndex = parseData.splice(x, 1);
-    console.log(spliceSelectedIndex, "---delete 2---", parseData)
+    console.log(spliceSelectedIndex, "---delete 2---", parseData);
 
-    // I need to reassign the Ids to each of the remaining notes
-    parseData.forEach((item, i) => {
-      item.id = i + 0;
-    });
-    console.log('---delete 4---',parseData)
+    if(parseData === []) {
+      console.log('---detele 3---', parseData)
+      return writeFile(parseData)
+    } 
+    else {
+      // I need to reassign the Ids to each of the remaining notes
+      parseData.forEach((item, i) => {
+        item.id = i;
+      });
+      console.log("---delete 4---", parseData);
+  
+      // turn all the new data into a string for write file to work
+      const stringNote = JSON.stringify(parseData);
+      console.log("---delete 5---", stringNote);
 
-    // turn all the new data into a string for write file to work
-    const stringNote = JSON.stringify(parseData);
-    console.log("---delete 5---", stringNote);
-    
-
-    // write the updates notes back to the file
-    fs.writeFile("./db/db.json", stringNote, (writeErr) =>
-      writeErr
-        ? console.error(writeErr)
-        : console.log("---deleted note successfully---")
-    );
+      return writeFile(stringNote)
+    }
   });
 });
 
